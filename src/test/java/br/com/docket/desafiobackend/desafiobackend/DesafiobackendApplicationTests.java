de58cac9fcb8941d3d739fae1486928754f3682c
alexaugustobr <alexcologia33@gmail.com>
2020-07-14 17:53:56 -0300

    inicio

---
+++ src/test/java/br/com/docket/desafiobackend/desafiobackend/DesafiobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.docket.desafiobackend.desafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
